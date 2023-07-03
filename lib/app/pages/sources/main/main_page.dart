b0b422d323b5f1d5a7f7bee80a5c93a2ed4c915e
bleidertcs <bleider.colina@tcs.com.ve>
2023-07-03 14:38:09 -0400

    agregado icono de telemedicina

--- lib/app/pages/sources/main/main_page.dart
+++ lib/app/pages/sources/main/main_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../shared/method/back_button_action.dart';
 import '/app/pages/widgets/common_widgets.dart';
 import '/app/pages/widgets/title_bar_widget.dart';
 import 'widgets/main_widgets.dart';
@@ -25,32 +26,35 @@ class _MainPageState extends State<MainPage> with MainWidgets {
   Widget build(BuildContext context) {
     //MainBloc userMainBloc = BlocProvider.of<MainBloc>(context);
     //userMainBloc.add(const ShowHomeServiceAssignedEvent());
-    return SafeArea(
-      child: Scaffold(
-        appBar: generateAppBar(context: context),
-        body: MultiBlocListener(
-          listeners: [
-            //NavigationBloc y LogoutBloc comunes en todas las paginas.
-            AppCommonWidgets.listenerNavigationBloc(),
-            AppCommonWidgets.listenerLogoutBloc()
-          ],
-          child: Container(
-            decoration: const BoxDecoration(color: Colors.white),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: Column(
-                children: [
-                  SizedBox(height: dataSpaceHeight),
-                  createDoctorInfo(context: context),
-                  SizedBox(height: dataSpaceHeight),
-                  serviceAvailabilityDashboard(context: context),
-                  SizedBox(height: dataSpaceHeight),
-                  const TitleBar(title: 'Atenciones Pendientes:'),
-                  const SizedBox(
-                    height: 20.0,
-                  ),
-                  amdInformationAssigned(context: context)
-                ],
+    return WillPopScope(
+      onWillPop: () async => backButtonActions(),
+      child: SafeArea(
+        child: Scaffold(
+          appBar: generateAppBar(context: context),
+          body: MultiBlocListener(
+            listeners: [
+              //NavigationBloc y LogoutBloc comunes en todas las paginas.
+              AppCommonWidgets.listenerNavigationBloc(),
+              AppCommonWidgets.listenerLogoutBloc()
+            ],
+            child: Container(
+              decoration: const BoxDecoration(color: Colors.white),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Column(
+                  children: [
+                    SizedBox(height: dataSpaceHeight),
+                    createDoctorInfo(context: context),
+                    SizedBox(height: dataSpaceHeight),
+                    serviceAvailabilityDashboard(context: context),
+                    SizedBox(height: dataSpaceHeight),
+                    const TitleBar(title: 'Atenciones Pendientes:'),
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+                    amdInformationAssigned(context: context)
+                  ],
+                ),
               ),
             ),
           ),
