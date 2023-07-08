1b6a7e05f102949f5a957bbb5762b8b6d645d6bd
argenis-mendez <argenis.men20@gmail.com>
2023-07-08 15:25:27 -0400

    eliminados por no hacer falta

--- lib/app/api/services/reason_rejection_service.dart
+++
@@ -1,5 +0,0 @@
-import 'package:geolocalizacionamd/app/api/mappings/reason_rejection_mapping.dart';
-
-abstract class ReasonRejectionService {
-  Future<ReasonRejectionMapping> getReasonRejection(final String tokenUser);
-}
