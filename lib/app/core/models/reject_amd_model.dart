5a80ae2d74cc312d37e5697d62d8f8262c73fa0c
argenis-mendez <argenis.men20@gmail.com>
2023-07-08 17:00:20 -0400

    adicion de archivo git add reject_amd_model

---
+++ lib/app/core/models/reject_amd_model.dart
@@ -0,0 +1,10 @@
+class RejectAmdModel {
+  int idHomeServiceAttention;
+  String comment;
+  int idReasonReject;
+
+  RejectAmdModel(
+      {required this.idHomeServiceAttention,
+      required this.comment,
+      required this.idReasonReject});
+}
