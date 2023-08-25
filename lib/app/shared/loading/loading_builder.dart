31fe61ae144c9ec5dc65f7aed34601737b7a7f03
bleidertcs <bleider.colina@tcs.com.ve>
2023-08-25 11:47:43 -0400

    ajuste de estilo en el circular progress indicator

--- lib/app/shared/loading/loading_builder.dart
+++ lib/app/shared/loading/loading_builder.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import '/app/pages/styles/app_styles.dart';
 import '/app/shared/loading/loading_indicator.dart';
+import 'package:geolocalizacionamd/app/extensions/localization_ext.dart';
 
 class LoadingBuilder {
   LoadingBuilder(this.context);
@@ -17,14 +18,27 @@ class LoadingBuilder {
           return WillPopScope(
             onWillPop: () async => false,
             child: AlertDialog(
+              title: Text(
+                context.appLocalization.titleHeadingLoading,
+                style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                      fontWeight: FontWeight.bold,
+                      color: const Color(0xff2B5178),
+                    ),
+                textAlign: TextAlign.center,
+              ),
+              titlePadding: const EdgeInsets.all(16),
+              alignment: Alignment.center,
               shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                  side: BorderSide(
-                      color: AppStyles.colorBluePrimary,
-                      style: BorderStyle.solid,
-                      width: 4.0)),
-              backgroundColor: AppStyles.colorGray,
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                side: BorderSide(
+                  color: AppStyles.colorBluePrimary,
+                  style: BorderStyle.solid,
+                  width: 2.0,
+                ),
+              ),
+              backgroundColor: AppStyles.colorWhite,
               content: LoadingIndicator(menssage: description),
+              contentPadding: const EdgeInsets.all(16),
             ),
           );
         });
